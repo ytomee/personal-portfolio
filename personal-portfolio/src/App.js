31d9ddc265e0e685efd14249066f975e47798f3c
Tomé Almeida <rltomealmeida@gmail.com>
2024-01-06 11:33:18 +0000

    about

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css';
 import Header from './components/header/Header';
-import Home from './components/home/Home'
+import Home from './components/home/Home';
+import About from './components/about/About';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
 
     <main className='main'>
       <Home />
+      <About />
     </main>
     </>
   )

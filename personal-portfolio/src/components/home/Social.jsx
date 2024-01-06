5d4c67359c98b71e3c7394d31428c1bb9d1b7c90
Tomé Almeida <rltomealmeida@gmail.com>
2024-01-06 10:53:06 +0000

    first upload

---
+++ personal-portfolio/src/components/home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home_social">
+        <a href="https://www.instagram.com/tome.almeida/" className="home_social-icon" 
+        target="_blank" rel='noreferrer'>
+            <i className="uil uil-instagram"></i>
+        </a>
+        
+        <a href="https://wa.me/+351965360269" className="home_social-icon" 
+        target="_blank" rel='noreferrer'>
+            <i className="uil uil-whatsapp"></i>
+        </a>
+        
+        <a href="https://github.com/ytomee" className="home_social-icon" 
+        target="_blank" rel='noreferrer'>
+            <i className="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social

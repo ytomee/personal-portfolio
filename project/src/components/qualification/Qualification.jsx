2c3af052486a092d3d7f144bbad463fe722a5fbd
Tomé Almeida <rltomealmeida@gmail.com>
2024-01-14 23:12:38 +0000

    school update

--- project/src/components/qualification/Qualification.jsx
+++ project/src/components/qualification/Qualification.jsx
@@ -40,12 +40,13 @@ const Qualification = () => {
                 <div className={toggleState === 1 ? 
                     "qualification_content qualification_content-active" :
                     "qualification_content"}>
+
                     <div className="qualification_data">
                         <div>
-                            <h3 className="qualification_title">ETPC</h3>
-                            <span className="qualification_subtitle"> Portugal - School</span>
+                            <h3 className="qualification_title">EB1 Murtede</h3>
+                            <span className="qualification_subtitle"> Portugal - Murtede</span>
                             <div className="qualification_calender">
-                                <i className="uil uil-calendar-alt"></i> 2022 - Present
+                                <i className="uil uil-calendar-alt"></i> 2011 - 2015
                             </div>
                         </div>
 
@@ -62,10 +63,41 @@ const Qualification = () => {
                             <span className="qualification_line"></span>
                         </div>
                         <div>
-                            <h3 className="qualification_title">Art Director</h3>
-                            <span className="qualification_subtitle"> Portugal - School</span>
+                            <h3 className="qualification_title">Pedro Teixeira</h3>
+                            <span className="qualification_subtitle"> Portugal - Cantanhede</span>
                             <div className="qualification_calender">
-                                <i className="uil uil-calendar-alt"></i> 2021 - Present
+                                <i className="uil uil-calendar-alt"></i> 2015 - 2017
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="qualification_data">
+                        <div>
+                            <h3 className="qualification_title">Lima-de-Faria</h3>
+                            <span className="qualification_subtitle"> Portugal - Cantanhede</span>
+                            <div className="qualification_calender">
+                                <i className="uil uil-calendar-alt"></i> 2017 - 2022
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="qualification_rounder"></span>
+                            <span className="qualification_line"></span>
+                        </div>
+                    </div>
+                    
+                    <div className="qualification_data">
+                        <div></div>
+                        <div>
+                            <span className="qualification_rounder"></span>
+                            <span className="qualification_line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification_title">ETPC</h3>
+                            <span className="qualification_subtitle"> Portugal - Cantanhede</span>
+                            <div className="qualification_calender">
+                                <i className="uil uil-calendar-alt"></i> 2022 - Present
                             </div>
                         </div>
 
